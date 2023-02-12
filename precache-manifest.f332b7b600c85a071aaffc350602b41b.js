ace28250280e1e41edb9bd6107744321559273fc
Mukund <Mukund>
2023-02-13 00:26:16 +0530

    Updates

---
+++ precache-manifest.f332b7b600c85a071aaffc350602b41b.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "8de5022d22e7e0f99fff",
+    "url": "/Mukund604/medium.git/static/css/main.06479445.chunk.css"
+  },
+  {
+    "revision": "8de5022d22e7e0f99fff",
+    "url": "/Mukund604/medium.git/static/js/main.8de5022d.chunk.js"
+  },
+  {
+    "revision": "561126de61029355b316",
+    "url": "/Mukund604/medium.git/static/js/1.561126de.chunk.js"
+  },
+  {
+    "revision": "bcfa6c1ce8eb3ecd754a",
+    "url": "/Mukund604/medium.git/static/js/2.bcfa6c1c.chunk.js"
+  },
+  {
+    "revision": "f774608df9488db9314c",
+    "url": "/Mukund604/medium.git/static/js/runtime~main.f774608d.js"
+  },
+  {
+    "revision": "a921d0343ae89048e1e3060fa712408b",
+    "url": "/Mukund604/medium.git/static/media/logo1.a921d034.png"
+  },
+  {
+    "revision": "25ab0e5340bcade48ed0f64995ee8eac",
+    "url": "/Mukund604/medium.git/static/media/binaryAnimation.25ab0e53.gif"
+  },
+  {
+    "revision": "dbc18f11bd56dcb4218af5fc7841aac8",
+    "url": "/Mukund604/medium.git/static/media/logo.dbc18f11.png"
+  },
+  {
+    "revision": "a71ed72b9c7f614d57faa5302e1c3278",
+    "url": "/Mukund604/medium.git/static/media/UnlimitedReading.a71ed72b.svg"
+  },
+  {
+    "revision": "3e6faca2b3a957d1846b48c65a4f5ac7",
+    "url": "/Mukund604/medium.git/static/media/Support.3e6faca2.svg"
+  },
+  {
+    "revision": "005084cf6a77ede12afa03abbe103ee8",
+    "url": "/Mukund604/medium.git/index.html"
+  }
+];
